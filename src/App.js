897242b03d4d4473bb04a055ae529ebc56a258db
dannielum <dannielum@gmail.com>
2021-10-07 16:57:42 -0400

    restructure layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import Header from './layout/Header';
+import Footer from './layout/Footer';
 import Dashboard from './pages/Dashboard';
+
+const mdTheme = createTheme();
 
 function App() {
   return (
-    <div>
-      <Dashboard />
-    </div>
+    <ThemeProvider theme={mdTheme}>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <Header />
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+          }}
+        >
+          <Toolbar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            <Dashboard />
+            <Footer sx={{ pt: 4 }} />
+          </Container>
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
 }
 
